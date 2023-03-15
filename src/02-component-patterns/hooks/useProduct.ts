46ff3cc3d64150b88ee335826f33e55f6f1b8ac2
RogerBC <roger@zarego.com>
2023-03-15 00:57:48 -0300

    Control props implemented alt.
    Signed-off-by: RogerBC <roger@zarego.com>

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useProductArg } from '../interfaces';
 
 const INITIAL_VALUE: number = 0;
@@ -9,16 +9,11 @@ export const useProduct = ({
 }: useProductArg) => {
   const [count, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   useEffect(() => {
     setCounter(value);
   }, [value]);
 
   const increaseBy = (value: number): void => {
-    if (isControlled) {
-      return onChange!({ count: value, product });
-    }
     const newValue = Math.max(count + value, INITIAL_VALUE);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -14,35 +14,18 @@ export const useShoppingCart = () => {
     product: Product;
   }) => {
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        //* This is another method to be removed.
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return { ...rest };
       }
-      //* This is another method to be removed.
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return { ...rest };
-
-      //! This is another method
-      // if (count === 0) {
-      //   //* This is another method to be removed.
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      //   return { ...rest };
-      // }
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: {
-      //     ...product,
-      //     count,
-      //   },
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]: {
+          ...product,
+          count,
+        },
+      };
     });
   };
   return {
